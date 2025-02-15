077c9fab9f0ca9a582ee4b3e2af48676f00c7c2e
Igor S <145506489+searaigor@users.noreply.github.com>
2025-02-15 18:22:17 -0300

    Update function.js
    finalizei implementação do botao de troca de cartao de cinza pra roxo

--- function.js
+++ function.js
@@ -490,61 +490,82 @@ trocarCinzaRoxo.addEventListener('click', async () => {
     
     // Cria um novo documento PDF em branco
     const doc = new jsPDF();
+
+    const textoObs = "Estou ciente que esse cartão ficara retido no ato da solicitação da troca para confecção da segunda via, que será produzida em até 2 dias úteis para retirada no CEAC Parque Shopping ou CEAC Rodoviária Nova, Aracaju/SE."
+
+    const textoAssinatura = `Aracaju/SE, ${dataHojeFormatada}.\n\n\n\n\n________________________________________________\nAssinatura do Requerente`;
+
+    const textoAssinaturaCoordenador = `________________________________________________\nMARIA SOLANGE DA SILVA`;
+
+    const textoObsCoordenador = "Chefe do Núcleo de Atendimento - SMTT"
+
+    const textoEndereco = "______________________________________________________________\nSMTT, Rua Roberto Fonseca, 200, Inacio Barbosa, Aracaju-SE\nFone: (79) 98836-6435 e 98836-6497";
+
+    //Adiciona a imagem do logo
+    const logoURL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm_F4EJslF8WhpMy0bCSb7D3aJpiYcZbpxxb0_-4KfgKzvUMEtr6yYQc9RL-TfC44m5-4&usqp=CAU';
+    const logoImg = await carregarImagem(logoURL);
+    doc.addImage(logoImg, 'PNG', 10, 10, 50, 20);
+
+    // Adiciona o texto institucional ao lado da imagem
+    doc.setFontSize(9);
+
+
+    let textY = 13;
+    textLines.forEach(line => {
+        doc.text(line, 65, textY);
+        textY += 3;
+    });
+
+    doc.setFontSize(12);
+    doc.setFont("helvetica", "bold");
+    adicionarTextoJustificado(doc, textoObs, 170, 20, 105);
+    doc.setFontSize(15);
+    doc.text("REQUERIMENTO", 100, 40, { align: 'center' });
+    doc.setFont("helvetica", "normal");
+    doc.setFontSize(12);
+    doc.text(textoAssinatura, 100, 130, { align: 'center' });
+    doc.setFontSize(9);
+    doc.text(textoEndereco, 100, 278, { align: 'center' });
+
     
     if(autorizacao2){
 
-        const textoPrincipal = `O Núcleo de Atendimento da Perícia Médica com base nas Leis n° 1.723/91 e n° 1.325/1987 comunica que o(a) requerente ${nome} portador(a) do RG nº ${rg} e inscrito(a) no CPF sob o nº ${cpf}, residente e domiciliado na ${endereco},${numCasa},${bairro},${cidade}, telefone nº ${telefone1Formatado}, solicita a substituição do Cartão Mais Aracaju Gratuidade da COR CINZA de nº ${numeroCartao} para a COR ROXA que NÃO PERMITE o acesso à parte traseira dos ônibus mediante a validação da biometria facial do beneficiário.`
+        const textoPrincipal = `O Núcleo de Atendimento da Perícia Médica com base nas Leis n° 1.723/91 e n° 1.325/1987 comunica que o(a) requerente ${nome} portador(a) do RG nº ${rg} SSP ${ssp} e inscrito(a) no CPF sob o nº ${cpf}, residente e domiciliado na ${endereco}, ${numCasa}, ${bairro}, ${cidade}, telefone nº ${telefone1Formatado}, solicita a substituição do Cartão Mais Aracaju Gratuidade da COR CINZA de nº ${numeroCartao} para a COR ROXA que NÃO PERMITE o acesso à parte traseira dos ônibus mediante a validação da biometria facial do beneficiário.`
 
-        const textoObs = "Estou ciente que esse cartão ficara retido no ato da solicitação da troca para confecção da segunda via, que será produzida em até 2 dias úteis para retirada no CEAC Parque Shopping ou CEAC Rodoviária Nova, Aracaju/SE."
-
-        const textoAssinatura = `Aracaju/SE, ${dataHojeFormatada}.\n\n\n\n\n________________________________________________\nAssinatura do Requerente`;
-
-        const textoAssinaturaCoordenador = `________________________________________________\nMARIA SOLANGE DA SILVA`;
-
-        const textoObsCoordenador = "Chefe do Núcleo de Atendimento - SMTT"
-
-        const textoEndereco = "____________________________________________________________________\nRua Roberto Fonseca, 200, Inacio Barbosa, Aracaju-SE\nFone: (79) 98836-6435 e 98836-6497";
 
         doc.setFontSize(12);
         adicionarTextoJustificado(doc, textoPrincipal, 170, 20, 50);
-        doc.setFont("helvetica", "bold");
-        adicionarTextoJustificado(doc, textoObs, 170, 20, 105);
-        doc.setFontSize(15);
-        doc.text("REQUERIMENTO", 100, 40, { align: 'center' });
-        doc.setFont("helvetica", "normal");
-        doc.setFontSize(12);
-        doc.text(textoAssinatura, 100, 130, { align: 'center' });
+
         doc.text(textoAssinaturaCoordenador, 100, 205, { align: 'center' });
         doc.setFontSize(10);
         doc.text(textoObsCoordenador, 100, 215, { align: 'center' });
-        doc.text(textoEndereco, 100, 278, { align: 'center' });
+        
 
-
-        //Adiciona a imagem do logo
-        const logoURL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm_F4EJslF8WhpMy0bCSb7D3aJpiYcZbpxxb0_-4KfgKzvUMEtr6yYQc9RL-TfC44m5-4&usqp=CAU';
-        const logoImg = await carregarImagem(logoURL);
-        doc.addImage(logoImg, 'PNG', 10, 10, 50, 20);
-
-        // Adiciona o texto institucional ao lado da imagem
-        doc.setFontSize(9);
-    
-
-        let textY = 13;
-        textLines.forEach(line => {
-            doc.text(line, 65, textY);
-            textY += 3;
-        });
     }else{
-        nomeRequerente = prompt("Informe o nome do requerente:");
-        cpfRequerente = prompt("Informe o CPF do requerente:");
-        rgRequerente = prompt("Informe o RG do requerente:");
-        sppRequerente = prompt("SSP:");
+        const nomeRequerente = prompt("Informe o nome do requerente:");
+        const cpfRequerente = prompt(`Informe o CPF de ${nomeRequerente}:`);
+        const rgRequerente = prompt(`Informe o RG de ${nomeRequerente}:`);
+        const sspRequerente = prompt("SSP:");
+
+        const textoPrincipal1 = `O Núcleo de Atendimento da Perícia Médica com base nas Leis n° 1.723/91 e n° 1.325/1987 comunica que o(a) requerente ${nomeRequerente} portador(a) do RG nº ${rgRequerente} SSP ${sspRequerente} e inscrito(a) no CPF sob o nº ${cpfRequerente}, residente e domiciliado na ${endereco}, ${numCasa}, ${bairro}, ${cidade}, telefone nº ${telefone1Formatado}, AUTORIZO a substituição do Cartão Mais Aracaju Gratuidade da COR CINZA de nº ${numeroCartao} para a COR ROXA que NÃO PERMITE o acesso à parte traseira dos ônibus mediante a validação da biometria facial do beneficiário.`
+
+        doc.setFontSize(12);
+        adicionarTextoJustificado(doc, textoPrincipal1, 170, 20, 50);
+
+        doc.text(`Para preenchimento da SMTT:\n\nNome do beneficiário: ${nome}\nCPF: ${cpf}`, 15, 185);
+
+        doc.text(textoAssinaturaCoordenador, 100, 235, { align: 'center' });
+        doc.setFontSize(10);
+        doc.text(textoObsCoordenador, 100, 245, { align: 'center' });
+        
+
+
+
+
     }
-    
-    
-    // Salva o PDF com o nome desejado
-    const nomeArquivo = 'documento_em_branco.pdf';
-    doc.save(nomeArquivo);
+    // Salva o PDF com o nome personalizado
+    const nomeArquivoTrocaCartaoCinzaRoxo = `${nome.replace(/ /g, '_')}_${cpf}.pdf`;
+    doc.save(nomeArquivoTrocaCartaoCinzaRoxo);
 });
 
    
